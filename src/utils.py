58dd0dbe140bd15b9aa57a2dd1ac81fcd1e42e73
Alex Biehl <ab7289@nyu.edu>
2022-12-04 17:11:23 -0500

    added utilities

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ from typing import Tuple
 
 import torch
 from torch import nn
+from torch_geometric.utils import normalized_cut
 
 
 def epoch_time(start_time, end_time):
@@ -21,3 +22,18 @@ def count_parameters(model: nn.Module) -> Tuple[int, int]:
         if p.requires_grad:
             num_parameters_requiring_grad += numel
     return num_parameters, num_parameters_requiring_grad
+
+
+def normalized_cut_2d(edge_index: torch.Tensor, pos: torch.Tensor):
+    """
+    Courtesy of
+    https://github.com/pyg-team/pytorch_geometric/blob/master/examples/mnist_graclus.py
+    """
+    row, col = edge_index
+    edge_attr = torch.norm(pos[row] - pos[col], p=2, dim=1)
+    return normalized_cut(edge_index, edge_attr, num_nodes=pos.size(0))
+
+
+def calculate_accuracy(outputs: torch.Tensor, targets: torch.Tensor):
+    pred = outputs.max(1)[1]
+    return pred.eq(targets).sum().item()
