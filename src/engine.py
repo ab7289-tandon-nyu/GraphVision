5790b129912be1e7523bfee543de074db4ffecda
Alex Biehl <ab7289@nyu.edu>
2022-12-04 17:11:41 -0500

    added basic engine utilities

---
+++ src/engine.py
@@ -0,0 +1,42 @@
+from typing import Any, Optional
+
+from utils import calculate_accuracy
+
+
+def train(model, iter, criterion, optimizer, device, scheduler: Optional[Any] = None):
+    model.train()
+
+    epoch_loss = 0
+    epoch_acc = 0
+    for data in iter:
+        data = data.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(data)
+        loss = criterion(outputs, data.y)
+        loss.backward()
+
+        optimizer.step()
+        if scheduler is not None:
+            scheduler.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += calculate_accuracy(outputs, data.y)
+    return epoch_loss / len(iter), epoch_acc / len(iter)
+
+
+def evaluate(model, iter, criterion, device):
+    model.eval()
+
+    epoch_loss = 0
+    epoch_acc = 0
+    for data in iter:
+        data = data.to(device)
+
+        outputs = model(data)
+        loss = criterion(outputs, data.y)
+
+        epoch_loss += loss.item()
+        epoch_acc += calculate_accuracy(outputs, data.y)
+    return epoch_loss / len(iter), epoch_acc / len(iter)
